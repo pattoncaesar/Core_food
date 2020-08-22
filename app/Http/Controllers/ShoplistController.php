b790b5589c793b5e9e7d349611d5fc7153c3aed8
ChangKaiJie <s1318020@ntut.org.tw>
2020-08-22 16:28:59 +0800

    ~Shoplist local_id

--- app/Http/Controllers/ShoplistController.php
+++ app/Http/Controllers/ShoplistController.php
@@ -8,6 +8,7 @@ use App\FoodMain;
 use App\ShopMain;
 use DB;
 use Illuminate\Http\Request;
+
 /*
  * Shop -> Area, Local
  * shop_name, shop_text, main_food, open_time, address
@@ -18,19 +19,35 @@ class ShoplistController extends Controller
 {
     public function index($area_id, $local_id = null)
     {
-        DB::connection()->enableQueryLog();
-
+        //  default 台北市
         if (is_null($area_id) || $area_id < 1 || $area_id > 18) $area_id = 1;
+
+        //  validate local_id
+        $local_of_area = AreaSub::where('master_id', '=', $area_id)
+                ->where('id', '=', $local_id);
+        if ($local_of_area->count() != 1)   $local_id = null;
+
+        //  Info for this area
         $area = AreaMain::find($area_id);
+
+        //  arealist, foodlist for right side menu
         $area_list = AreaMain::orderBy('order', 'asc')->get();
         $food_list = FoodMain::orderBy('order', 'asc')->get();
+
         //  TODO: Move to Repository
         //  User Story
         //  AreaMain 1-m ShopMain
         //  +FoodMain
 
 //        $t = AreaMain::find($area_id)->shopmains;
-        $shop = ShopMain::where('main_area', '=', $area_id)->paginate(20);
+        if ($local_id) {
+            $shop = ShopMain::where('main_area', '=', $area_id)
+                ->where('sub_area', '=', $local_id)
+                ->paginate(20);
+        } else {
+            $shop = ShopMain::where('main_area', '=', $area_id)
+                ->paginate(20);
+        }
 
         return view('shoplist',
             [
--- resources/views/shoplist.blade.php
+++ resources/views/shoplist.blade.php
@@ -7,10 +7,6 @@
     <link href="{{ URL::asset('/css/common.css') }}" rel="stylesheet">
     <link href="{{ URL::asset('/css/search.css') }}" rel="stylesheet">
 
-    <script src="{{ URL::asset('/js/jquery.min.js') }}"></script>
-    <script src="{{ URL::asset('/js/jquery-migrate-3.0.min.js') }}"></script>
-    <script src="{{ URL::asset('/js/js.cookie.js') }}"></script>
-
     <title>美食查詢-{{$area->area_name}}-</title>
 </head>
 <body>
@@ -27,7 +23,7 @@
         <li>></li>
         <li><a href="/shoplist/{{$area->id}}">{{$area->area_name}}</a></li>
         <li>></li>
-        <?php if (isset($sub_area)&&$sub_area!=null): ?>
+        <?php if (isset($sub_area) && $sub_area != null): ?>
         <li><a href="/shoplist/{main_area}/{sub_area}">{area_main_name}・{area_subs_name}</a></li>
         <li>></li>
         <?php endif ?>
@@ -36,61 +32,61 @@
 </div>
 <div class="f-mainContents u-container">
     <div class="mainWrap">
-        <?php if (isset($area_main_name)&&$area_main_name!=null): ?>
+        <?php if (isset($area_main_name) && $area_main_name != null): ?>
         <h2>{query} 查詢結果</h2>
         <?php else: ?>
         <h2>查詢結果</h2>
         <?php endif ?>
 
         <div class="m-pager">
-{{--            {!! $shop->links('pagination.default') !!}--}}
+            {{--            {!! $shop->links('pagination.default') !!}--}}
             {!! $shop->links() !!}
         </div>
         <div class="m-shopList">
             <ul>
                 @foreach ($shop as $shop_item)
-                <li class="shopData">
-                    <div class="shopHead">
-                        <a href="/shop/{{$shop_item->id}}">{{$shop_item->shop_name}}</a>
-                    </div>
-                    <div class="shopDetail">
-                        <div class="photo">
-                            <img src="{{ URL::asset('/img/shop/{photo_num}.png') }}" alt="">
+                    <li class="shopData">
+                        <div class="shopHead">
+                            <a href="/shop/{{$shop_item->id}}">{{$shop_item->shop_name}}</a>
                         </div>
-                        <div class="info">
-                            <div class="shopTitle">
-                                <p>{{$shop_item->main_title}}</p>
+                        <div class="shopDetail">
+                            <div class="photo">
+                                <img src="{{ URL::asset('/img/shop/{photo_num}.png') }}" alt="">
                             </div>
-                            <table>
-                                <tr>
-                                    <th>分類</th>
-                                    <td>{{$shop_item->foodmain->food_name}}</td>
-                                </tr>
-                                <tr>
-                                    <th>標籤</th>
-                                    <td>
-                                        <ul class="foodTags">
-                                            @foreach ($shop_item->foodTags as $food_tag)
-                                            <li>{{$food_tag->foodsub->food_name}}</li>
-                                            @endforeach
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>營業時間</th>
-                                    <td>{{$shop_item->open_time}}</td>
-                                </tr>
-                                <tr>
-                                    <th>地址</th>
-                                    <td>{{$shop_item->address}}</td>
-                                </tr>
-                            </table>
-                            <div class="linkBtn">
-                                <button class="like u-hoverOpacity" value="{{$shop_item->id}}">LIKE</button>
+                            <div class="info">
+                                <div class="shopTitle">
+                                    <p>{{$shop_item->main_title}}</p>
+                                </div>
+                                <table>
+                                    <tr>
+                                        <th>分類</th>
+                                        <td>{{$shop_item->foodmain->food_name}}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>標籤</th>
+                                        <td>
+                                            <ul class="foodTags">
+                                                @foreach ($shop_item->foodTags as $food_tag)
+                                                    <li>{{$food_tag->foodsub->food_name}}</li>
+                                                @endforeach
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>營業時間</th>
+                                        <td>{{$shop_item->open_time}}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>地址</th>
+                                        <td>{{$shop_item->address}}</td>
+                                    </tr>
+                                </table>
+                                <div class="linkBtn">
+                                    <button class="like u-hoverOpacity" value="{{$shop_item->id}}">LIKE</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
+                    </li>
                 @endforeach
             </ul>
         </div>
@@ -103,23 +99,27 @@
             <p class="searchTitle"><span>查詢</span></p>
             <div class="searchArea">
                 <p class="typeTitle">地點</p>
-                <ul class="main">
+                <ul class="main metismenu" id="areaMenu">
                     @foreach ($area_list as $area_item)
-                    <li class="m-list is-active">
-                        <p class="mainList"><span>{{$area_item->area_name}}</span></p>
-                        <ul class="sub">
-                            <li>
-                                <input type="checkbox" id="s-area0" checked>
-                                <label for="s-area0">全{{$area_item->area_name}}</label>
-                            </li>
-                            @foreach ($area_item->subarealist->sortBy('order') as $area_sub)
-                            <li>
-                                <input type="checkbox" id="s-area{{$area_sub->id}}">
-                                <label for="s-area{{$area_sub->id}}">{{$area_sub->area_name}}</label>
-                            </li>
-                            @endforeach
-                        </ul>
-                    </li>
+                        @if ($loop->first)
+                        <li class="m-list is-active">
+                            @else
+                            <li class="m-list ">
+                            @endif
+                            <p class="mainList"><span>{{$area_item->area_name}}</span></p>
+                            <ul class="sub">
+                                <li>
+                                    <input type="checkbox" id="s-area0" checked>
+                                    <label for="s-area0">全{{$area_item->area_name}}</label>
+                                </li>
+                                @foreach ($area_item->subarealist->sortBy('order') as $area_sub)
+                                    <li>
+                                        <input type="checkbox" id="s-area{{$area_sub->id}}">
+                                        <label for="s-area{{$area_sub->id}}">{{$area_sub->area_name}}</label>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
                     @endforeach
                 </ul>
             </div>
@@ -127,21 +127,21 @@
                 <p class="typeTitle">分類</p>
                 <ul class="main">
                     @foreach ($food_list as $food_item)
-                    <li class="m-list is-active">
-                        <p class="mainList"><span>{{$food_item->food_name}}</span></p>
-                        <ul class="sub">
-                            <li>
-                                <input type="checkbox" id="s-genre0" checked>
-                                <label for="s-genre0">全{{$food_item->food_name}}</label>
-                            </li>
-                            @foreach ($food_item->subfoodlist->sortBy('order') as $food_sub)
-                            <li>
-                                <input type="checkbox" id="s-genre{{$food_sub->id}}">
-                                <label for="s-genre{{$food_sub->id}}">{{$food_sub->food_name}}</label>
-                            </li>
-                            @endforeach
-                        </ul>
-                    </li>
+                        <li class="m-list is-active">
+                            <p class="mainList"><span>{{$food_item->food_name}}</span></p>
+                            <ul class="sub">
+                                <li>
+                                    <input type="checkbox" id="s-genre0" checked>
+                                    <label for="s-genre0">全{{$food_item->food_name}}</label>
+                                </li>
+                                @foreach ($food_item->subfoodlist->sortBy('order') as $food_sub)
+                                    <li>
+                                        <input type="checkbox" id="s-genre{{$food_sub->id}}">
+                                        <label for="s-genre{{$food_sub->id}}">{{$food_sub->food_name}}</label>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
                     @endforeach
                 </ul>
             </div>
@@ -160,41 +160,38 @@
     </div>
 </footer>
 
-<script type="text/javascript" charset="utf-8">
-    $(window).load(function() {
-
-        $("button.like").each(function() {
+<script
+    src="https://code.jquery.com/jquery-3.5.1.js"
+    integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+    crossorigin="anonymous"></script>
+<script src="{{ URL::asset('/js/js.cookie.js') }}"></script>
+<script>
+    $(window).on('load', function () {
+        $("button.like").each(function () {
             id = $(this).attr("value");
-            if (typeof Cookies.get('shop_'+id+'_liked') === 'undefined'||Cookies.get('shop_'+id+'_liked')===0){
+            if (typeof Cookies.get('shop_' + id + '_liked') === 'undefined' || Cookies.get('shop_' + id + '_liked') === 0) {
                 //no cookie
                 $(this).removeClass('is-active');
-            }
-            else
-            {
+            } else {
                 $(this).addClass('is-active');
             }
         });
 
-
-
-
         $("button.like").on("click", function (evt) {
             id = $(evt.target).attr("value");
             //alert(Cookies.get('shop_'+id+'_liked'));
-            if (typeof Cookies.get('shop_'+id+'_liked') === 'undefined'||Cookies.get('shop_'+id+'_liked')===0){
+            if (typeof Cookies.get('shop_' + id + '_liked') === 'undefined' || Cookies.get('shop_' + id + '_liked') === 0) {
                 //no cookies
-                Cookies.set('shop_'+id+'_liked',1, { expires: 365 });
+                Cookies.set('shop_' + id + '_liked', 1, {expires: 365});
                 $(evt.target).addClass('is-active');
             } else {
                 //have cookies
                 $(evt.target).removeClass('is-active');
-                Cookies.remove('shop_'+id+'_liked');
+                Cookies.remove('shop_' + id + '_liked');
 
             }
         });
-
     });
 </script>
-
 </body>
 </html>
